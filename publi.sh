fc22700119bc32d4b58433a3707bd58bae86a101
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-05-27 13:07:14 +0300

    add travis support

---
+++ publi.sh
@@ -0,0 +1,9 @@
+# publi.sh
+# change the branch names appropriately
+# git checkout develop
+rm -rf _site/
+bundle jekyll build
+git add --all
+git commit -m "`date`"
+git push origin develop
+git subtree push --prefix  _site/ origin master
