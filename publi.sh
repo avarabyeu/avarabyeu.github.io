0b83653f20b11c0837ebb09bcc8a734ed2af7f81
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-05-27 14:33:10 +0300

    fix .gitignore

--- publi.sh
+++ publi.sh
@@ -4,10 +4,10 @@ set -e # Exit with nonzero exit code if anything fails
 # publi.sh
 # change the branch names appropriately
 # git checkout develop
-bundle install;
+git config --global user.email "andrei_varabyeu@gmail.com";
+git config --global user.name "avarabyeu";
+git remote set-url origin git@github.com:avarabyeu/avarabyeu.github.io.git;
 
-rm -rf _site/;
-bundle jekyll build
 git add --all;
 git commit -m "`date`";
 git push origin develop;
