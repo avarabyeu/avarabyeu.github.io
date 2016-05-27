c27938a06a12c6dfa4042ed56eadf1493e5e2957
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-05-27 13:58:32 +0300

    fix .gitignore

--- publi.sh
+++ publi.sh
@@ -4,8 +4,8 @@ set -e # Exit with nonzero exit code if anything fails
 # publi.sh
 # change the branch names appropriately
 # git checkout develop
-rm -rf _site/
-bundle jekyll build
+rm -rf _site/;
+"bundle jekyll build"
 git add --all
 git commit -m "`date`"
 git push origin develop
