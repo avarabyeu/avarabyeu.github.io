e6e441adbaf0f40fb0d7ef88d9cb60243b429bfd
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-05-28 11:41:15 +0300

    fix navigation template

--- publi.sh
+++ publi.sh
@@ -4,6 +4,8 @@ set -e # Exit with nonzero exit code if anything fails
 # publi.sh
 # change the branch names appropriately
 # git checkout develop
+git fetch;
+
 git config --global user.email "andrei_varabyeu@gmail.com";
 git config --global user.name "avarabyeu";
 git remote set-url origin git@github.com:avarabyeu/avarabyeu.github.io.git;
