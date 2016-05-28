a07504b9346f7e95b9455df3b1527bbc5fc963cc
Andrei Varabyeu <andrei_varabyeu@epam.com>
2016-05-28 11:53:48 +0300

    fix publish script

--- publi.sh
+++ publi.sh
@@ -4,12 +4,13 @@ set -e # Exit with nonzero exit code if anything fails
 # publi.sh
 # change the branch names appropriately
 # git checkout develop
-echo "Fetch the repo"
-git fetch;
 
 git config --global user.email "andrei_varabyeu@gmail.com";
 git config --global user.name "avarabyeu";
 git remote set-url origin git@github.com:avarabyeu/avarabyeu.github.io.git;
+
+echo "Fetch the repo"
+git fetch;
 
 git add _site;
 git commit -m "`date`";
