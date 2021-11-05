5a72080b3bccc1537f1e71b1cacee93cdea68b0d
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2021-11-05 12:26:58 +0300

    Fri Nov  5 12:26:58 +03 2021

--- publi.sh
+++ publi.sh
@@ -5,9 +5,9 @@ set -e # Exit with nonzero exit code if anything fails
 # change the branch names appropriately
 # git checkout develop
 
-git config --global user.email "andrei_varabyeu@gmail.com";
-git config --global user.name "avarabyeu";
-git remote set-url origin git@github.com:avarabyeu/avarabyeu.github.io.git;
+#git config --global user.email "andrei_varabyeu@gmail.com";
+#git config --global user.name "avarabyeu";
+#git remote set-url origin git@github.com:avarabyeu/avarabyeu.github.io.git;
 
 
 git add --force _site;
@@ -19,5 +19,5 @@ git commit -m "`date`";
 
 echo "Push subtree to master"
 git subtree split --prefix _site -b temp; # create a local temp branch containing the splitted output folder
-git push -f origin temp:master # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f origin temp:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
 git branch -D temp # delete the local gh-pages because you will need it: ref
